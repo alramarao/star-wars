ef6f871d26fef49148ff70178fe7a0c18ba33253
Ramarao Akula <ramarao.akula@valtech.com>
2023-06-16 22:45:09 +0200

    Added new component to display planet-details lazyLoad

---
+++ src/app/components/planet-details/planet-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-planet-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './planet-details.component.html',
+  styleUrls: ['./planet-details.component.scss']
+})
+export class PlanetDetailsComponent {
+
+}
