ba6affa86b3eb5ebff7d6326c5964f3d1577077f
Ramarao Akula <ramarao.akula@valtech.com>
2023-06-16 22:45:37 +0200

    Updated the routes for new components

--- src/app/routes/star-wars-routing.module.ts
+++ src/app/routes/star-wars-routing.module.ts
@@ -1,14 +1,39 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from '../pages/not-found/not-found.component';
+import { CharactersComponent } from '../components/characters/characters.component';
+
 export const StarWarsRoutes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'not-found',
+    redirectTo: 'characters',
+  },
+  {
+    path: 'characters',
+    component: CharactersComponent,
+  },
+  {
+    path: 'character/:uid',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import(
+            '../components/character-details/character-details.component'
+          ).then((m) => m.CharacterDetailsComponent),
+      },
+      {
+        path: 'planet/:pid',
+        loadComponent: () =>
+          import('../components/planet-details/planet-details.component').then(
+            (m) => m.PlanetDetailsComponent
+          ),
+      },
+    ],
   },
   {
     path: 'not-found',
-    component: NotFoundComponent
+    component: NotFoundComponent,
   },
   { path: '**', redirectTo: '/not-found' },
 ];
