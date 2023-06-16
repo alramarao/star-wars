7da4e575acc75793f472216172b1193ef944a482
Ramarao Akula <ramarao.akula@valtech.com>
2023-06-16 22:16:48 +0200

    Added the state providers in bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { StarWarsComponent } from './app/pages/star-wars/star-wars.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApiInterceptor } from './app/interceptors/api-interceptor.service';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { StarWarsEffects, StarWarsReducer } from './app/state';
 
 bootstrapApplication(StarWarsComponent, {
-  providers: [provideHttpClient(withInterceptors([ApiInterceptor]))],
+  providers: [
+    provideHttpClient(withInterceptors([ApiInterceptor])),
+    provideStore({ starWars: StarWarsReducer }),
+    provideEffects(StarWarsEffects),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: false, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+  ],
 }).catch((err) => console.error(err));
