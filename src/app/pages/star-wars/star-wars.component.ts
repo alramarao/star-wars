d0c10490b6cdeb2f7fe40e1de9fc2fa37f186bf1
Ramarao Akula <ramarao.akula@valtech.com>
2023-06-16 18:40:54 +0200

    Added new star-wars standalone component as root component

---
+++ src/app/pages/star-wars/star-wars.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-star-wars',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './star-wars.component.html',
+  styleUrls: ['./star-wars.component.scss']
+})
+export class StarWarsComponent {
+
+}
