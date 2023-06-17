d08a038e2ce901025641b5e6162b3cf6d32e6749
Ramarao Akula <ramarao.akula@valtech.com>
2023-06-17 21:47:13 +0200

    Added unit-test for not-found Component

---
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotFoundComponent } from './not-found.component';
+
+describe('NotFoundComponent', () => {
+	let component: NotFoundComponent;
+	let fixture: ComponentFixture<NotFoundComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [NotFoundComponent],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(NotFoundComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	describe('check for methods of NotFoundComponent', () => {
+		it('should create component', () => {
+			expect(component).toBeTruthy();
+		});
+	});
+});
